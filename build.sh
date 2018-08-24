86b4a23625e6f95e0ebd42839b560f5b01da23cb
Purinda Gunasekara <purinda.gunasekara@openbet.com>
2018-08-24 11:07:15 +1000

    Build process change

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ CWD=`python -c "import os; print(os.path.dirname(os.path.realpath(\"$0\")))"`
 # Tag/Version
 TAG="postgres"
 
+echo "Build process will start for Postgres Docker image version ${MAJOR}.${MINOR}..."
+echo ""
+sleep 3
+
 docker build -t "${TAG}:${MAJOR}.${MINOR}" \
     --build-arg PG_MAJOR="${MAJOR}" \
     --build-arg PG_MINOR="${MAJOR}.${MINOR}" \
